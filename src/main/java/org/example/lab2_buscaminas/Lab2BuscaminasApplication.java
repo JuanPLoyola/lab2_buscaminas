fdc50d71ccf31f7468ec7db0add0fa7b41ed9693
unknown <eduardo.carbajal@pucp.edu.pe>
2024-04-11 20:50:14 -0500

    primer commit

---
+++ src/main/java/org/example/lab2_buscaminas/Lab2BuscaminasApplication.java
@@ -0,0 +1,13 @@
+package org.example.lab2_buscaminas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2BuscaminasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2BuscaminasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/lab2_buscaminas/Lab2BuscaminasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab2_buscaminas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2BuscaminasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
